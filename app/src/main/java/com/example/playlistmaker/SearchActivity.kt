66646b136559dff4c043a2015273d92da89b84d2
Denis <d-e-n-2011@yandex.ru>
2025-04-17 02:07:54 +0300

    Добавил кнопку очистки текста на экран поиска и ее функционал

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -1,11 +1,44 @@
 package com.example.playlistmaker
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.EditText
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.appbar.MaterialToolbar
 
 class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        val toolbar = findViewById<MaterialToolbar>(R.id.search_toolbar)
+        toolbar.setNavigationOnClickListener {
+            finish()
+        }
+
+        val editText = findViewById<EditText>(R.id.edit_text_search)
+        val clearButton = findViewById<ImageView>(R.id.clear_button)
+        clearButton.setOnClickListener {
+            editText.text.clear()
+        }
+
+        val textWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                clearButton.visibility = if (s.isNullOrEmpty()) View.GONE else View.VISIBLE
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+        }
+
+        editText.addTextChangedListener(textWatcher)
     }
 }
