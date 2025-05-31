dae36311d99472689a5de51c482e259b61df613e
Denis <d-e-n-2011@yandex.ru>
2025-05-31 13:05:29 +0300

    Исправлено: убран закомментированный код

--- app/src/main/java/com/example/playlistmaker/PlayerActivity.kt
+++ app/src/main/java/com/example/playlistmaker/PlayerActivity.kt
@@ -15,13 +15,7 @@ import com.google.android.material.appbar.MaterialToolbar
 class PlayerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        enableEdgeToEdge()
         setContentView(R.layout.activity_player)
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-//            insets
-//        }
 
         val toolbar: MaterialToolbar = findViewById(R.id.player_toolbar)
 
