21a00fd9bf29624dbbafd1ee6e63b47babda870e
Denis <d-e-n-2011@yandex.ru>
2025-10-05 01:12:25 +0300

    Обновление таймера через корутину

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,4 +60,5 @@ dependencies {
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
     implementation(libs.androidx.fragment.ktx)
+    implementation(libs.kotlinx.coroutines.android)
 }
